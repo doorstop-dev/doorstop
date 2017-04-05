f315320e9e040fdf01e5f37c07c0622c612d900d
Jim MacArthur <jim.macarthur@codethink.co.uk>
2017-04-05 15:12:24 +0100

    Set baseurl to nothing if it's not already set

--- doorstop/core/publisher.py
+++ doorstop/core/publisher.py
@@ -500,7 +500,8 @@ def _lines_html(obj, linkify=False, extensions=EXTENSIONS,
             bottle.TEMPLATE_PATH.insert(0,
                                         os.path.join(os.path.dirname(__file__),
                                                      '..', '..', 'views'))
-
+            if 'baseurl' not in bottle.SimpleTemplate.defaults:
+                bottle.SimpleTemplate.defaults['baseurl'] = ''
             html = bottle_template(template, body=body, toc=toc_html, parent=obj.parent)
         except Exception:
             log.error("Problem parsing the template %s", template)
