d53986750db4f3e142b3644bf8f708d016c8120c
Jim MacArthur <jim+git@mode7.co.uk>
2017-04-11 16:28:25 +0100

    Find assets directory relative to publisher.py instead of sidebar.html

--- doorstop/core/publisher.py
+++ doorstop/core/publisher.py
@@ -67,7 +67,7 @@ def publish(obj, path, ext=None, linkify=None, index=None,
     # If publish html and then markdown ensure that the html template are still available
     if not template:
         template = HTMLTEMPLATE
-    template_assets = os.path.join(os.path.dirname(template), 'assets')
+    template_assets = os.path.join(os.path.dirname(__file__), 'files', 'assets')
     if os.path.isdir(template_assets):
         log.info("Copying %s to %s", template_assets, assets_dir)
         common.copy_dir_contents(template_assets, assets_dir)
