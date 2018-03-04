32ae42694e39db0626bc8325a5b011c3d39b1a77
Éric Larivière <ericlariviere@hotmail.com>
2018-03-03 19:41:35 -0500

    Make some display only field in the UI uneditable

--- doorstop/gui/widget.py
+++ doorstop/gui/widget.py
@@ -13,85 +13,29 @@ except ImportError as _exc:  # pragma: no cover (manual test)
     tk = Mock()
     ttk = Mock()
 
-# # Fonts
-fontNormal = font.Font(family='TkDefaultFont', size=1)
-fontFixed = font.Font(family='Courier New', size=1)
-
-# # Styles (http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/ttk-style-layer.html)
-
-# Style for Button
-styleDsTButton = ttk.Style()
-styleDsTButton.configure('ds.TButton', font=fontNormal)
-
-# Style for Checkbutton
-styleDsTCheckbutton = ttk.Style()
-styleDsTCheckbutton.configure('ds.TCheckbutton', font=fontNormal)
-
-# Style for Combobox
-styleDsTCombobox = ttk.Style()
-styleDsTCombobox.configure('ds.TCombobox', font=fontNormal)
-
-# Style for Entry
-styleDsTEntry = ttk.Style()
-styleDsTEntry.configure('ds.TEntry', font=fontNormal)
-
-# Style for Frame
-styleDsTFrame = ttk.Style()
-styleDsTFrame.configure('ds.TFrame', font=fontNormal)
-
-# Style for Label
-styleDsTLabel = ttk.Style()
-styleDsTLabel.configure('ds.TLabel', font=fontNormal)
-
-# Style for LabelFrame
-styleDsTLabelFrame = ttk.Style()
-styleDsTLabelFrame.configure('ds.TLabelFrame', font=fontNormal)
-
-# Style for Menubutton
-styleDsTMenubutton = ttk.Style()
-styleDsTMenubutton.configure('ds.TMenubutton', font=fontNormal)
-
-# Style for Notebook
-styleDsTNotebook = ttk.Style()
-styleDsTNotebook.configure('ds.TNotebook', font=fontNormal)
-
-# Style for PanedWindow
-styleDsTPanedwindow = ttk.Style()
-styleDsTPanedwindow.configure('ds.TPanedwindow', font=fontNormal)
-
-# Style for Progressbar
-styleDsHorizontalTProgressbar = ttk.Style()
-styleDsHorizontalTProgressbar.configure('ds.Horizontal.TProgressbar', font=fontNormal)
-styleDsVerticalTProgressbar = ttk.Style()
-styleDsVerticalTProgressbar.configure('ds.Vertical.TProgressbar', font=fontNormal)
-
-# Style for Radiobutton
-styleDsTRadiobutton = ttk.Style()
-styleDsTRadiobutton.configure('ds.TRadiobutton', font=fontNormal)
-
-# Style for Scale
-styleDsHorizontalTScale = ttk.Style()
-styleDsHorizontalTScale.configure('ds.Horizontal.TScale', font=fontNormal)
-styleDsVerticalTScale = ttk.Style()
-styleDsVerticalTScale.configure('ds.Vertical.TScale', font=fontNormal)
-
-# Style for Scrollbar
-styleDsHorizontalTScrollbar = ttk.Style()
-styleDsHorizontalTScrollbar.configure('ds.Horizontal.TScrollbar', font=fontNormal)
-styleDsVerticalTScrollbar = ttk.Style()
-styleDsVerticalTScrollbar.configure('ds.Vertical.TScrollbar', font=fontNormal)
-
-# Style for Separator
-styleDsTSeparator = ttk.Style()
-styleDsTSeparator.configure('ds.TSeparator', font=fontNormal)
-
-# Style for Sizegrip
-styleDsTSizegrip = ttk.Style()
-styleDsTSizegrip.configure('ds.TSizegrip', font=fontNormal)
-
-# Style for Treeview
-styleDsTreeview = ttk.Style()
-styleDsTreeview.configure('ds.Treeview', font=fontNormal)
+# # Styling
+fontNormal = None
+fontFixed = None
+styleDsTButton = None
+styleDsTCheckbutton = None
+styleDsTCombobox = None
+styleDsTEntry = None
+styleDsTFrame = None
+styleDsTLabel = None
+styleDsTLabelFrame = None
+styleDsTMenubutton = None
+styleDsTNotebook = None
+styleDsTPanedwindow = None
+styleDsHorizontalTProgressbar = None
+styleDsVerticalTProgressbar = None
+styleDsTRadiobutton = None
+styleDsHorizontalTScale = None
+styleDsVerticalTScale = None
+styleDsHorizontalTScrollbar = None
+styleDsVerticalTScrollbar = None
+styleDsTSeparator = None
+styleDsTSizegrip = None
+styleDsTreeview = None
 
 # # Widget
 
@@ -114,52 +58,156 @@ class _Listbox2(tk.Listbox):  # pragma: no cover (manual test), pylint: disable=
             self.config(width=width + shift)
 
 
-def Button(parent, **extra):
-    result = ttk.Button(parent, **extra, style="ds.TButton")
+def Button(parent, *args, **kwargs):
+    result = ttk.Button(parent, *args, **kwargs, style="ds.TButton")
     return result
 
 
-def Checkbutton(parent, **extra):
-    result = ttk.Checkbutton(parent, **extra, style="ds.TCheckbutton")
+def Checkbutton(parent, *args, **kwargs):
+    result = ttk.Checkbutton(parent, *args, **kwargs, style="ds.TCheckbutton")
     return result
 
 
-def Combobox(parent, **extra):
-    result = ttk.Combobox(parent, font=fontNormal, **extra)
+def Combobox(parent, *args, **kwargs):
+    result = ttk.Combobox(parent, font=fontNormal, *args, **kwargs)
     return result
 
 
-def Label(parent, **extra):
-    result = ttk.Label(parent, **extra)
+def Label(parent, *args, **kwargs):
+    result = ttk.Label(parent, *args, **kwargs)
     result.configure(font=fontNormal)
     return result
 
 
-def Listbox(parent, **extra):
-    result = tk.Listbox(parent, **extra)
+def Listbox(parent, *args, **kwargs):
+    result = tk.Listbox(parent, *args, **kwargs)
     result.configure(font=fontNormal)
     return result
 
 
-def Listbox2(parent, **extra):
-    result = _Listbox2(parent, **extra)
+def Listbox2(parent, *args, **kwargs):
+    result = _Listbox2(parent, *args, **kwargs)
     result.configure(font=fontNormal)
     return result
 
 
-def Entry(parent, **extra):
-    result = ttk.Entry(parent, **extra, font=fontFixed)
+def Entry(parent, *args, **kwargs):
+    result = ttk.Entry(parent, *args, **kwargs, font=fontFixed)
     return result
 
 
-def Text(parent, **extra):
-    result = tk.Text(parent, font=fontFixed, **extra)
+def Text(parent, *args, **kwargs):
+    result = tk.Text(parent, font=fontFixed, *args, **kwargs)
     return result
-    
 
-def Tk(root, **extra):
+
+def Tk():
+    result = tk.Tk()
+
+    # # Fonts
+    global fontNormal
+    fontNormal = font.Font(family='TkDefaultFont', size=1)
+    global fontFixed
+    fontFixed = font.Font(family='Courier New', size=1)
+
+    # # Styles (http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/ttk-style-layer.html)
+
+    # Style for Button
+    global styleDsTButton
+    styleDsTButton = ttk.Style()
+    styleDsTButton.configure('ds.TButton', font=fontNormal)
+
+    # Style for Checkbutton
+    global styleDsTCheckbutton
+    styleDsTCheckbutton = ttk.Style()
+    styleDsTCheckbutton.configure('ds.TCheckbutton', font=fontNormal)
+
+    # Style for Combobox
+    global styleDsTCombobox
+    styleDsTCombobox = ttk.Style()
+    styleDsTCombobox.configure('ds.TCombobox', font=fontNormal)
+
+    # Style for Entry
+    global styleDsTEntry
+    styleDsTEntry = ttk.Style()
+    styleDsTEntry.configure('ds.TEntry', font=fontNormal)
+
+    # Style for Frame
+    global styleDsTFrame
+    styleDsTFrame = ttk.Style()
+    styleDsTFrame.configure('ds.TFrame', font=fontNormal)
+
+    # Style for Label
+    global styleDsTLabel
+    styleDsTLabel = ttk.Style()
+    styleDsTLabel.configure('ds.TLabel', font=fontNormal)
+
+    # Style for LabelFrame
+    global styleDsTLabelFrame
+    styleDsTLabelFrame = ttk.Style()
+    styleDsTLabelFrame.configure('ds.TLabelFrame', font=fontNormal)
+
+    # Style for Menubutton
+    global styleDsTMenubutton
+    styleDsTMenubutton = ttk.Style()
+    styleDsTMenubutton.configure('ds.TMenubutton', font=fontNormal)
+
+    # Style for Notebook
+    global styleDsTNotebook
+    styleDsTNotebook = ttk.Style()
+    styleDsTNotebook.configure('ds.TNotebook', font=fontNormal)
+
+    # Style for PanedWindow
+    global styleDsTPanedwindow
+    styleDsTPanedwindow = ttk.Style()
+    styleDsTPanedwindow.configure('ds.TPanedwindow', font=fontNormal)
+
+    # Style for Progressbar
+    global styleDsHorizontalTProgressbar
+    styleDsHorizontalTProgressbar = ttk.Style()
+    styleDsHorizontalTProgressbar.configure('ds.Horizontal.TProgressbar', font=fontNormal)
+    global styleDsVerticalTProgressbar
+    styleDsVerticalTProgressbar = ttk.Style()
+    styleDsVerticalTProgressbar.configure('ds.Vertical.TProgressbar', font=fontNormal)
+
+    # Style for Radiobutton
+    global styleDsTRadiobutton
+    styleDsTRadiobutton = ttk.Style()
+    styleDsTRadiobutton.configure('ds.TRadiobutton', font=fontNormal)
+
+    # Style for Scale
+    global styleDsHorizontalTScale
+    styleDsHorizontalTScale = ttk.Style()
+    styleDsHorizontalTScale.configure('ds.Horizontal.TScale', font=fontNormal)
+    styleDsVerticalTScale = ttk.Style()
+    styleDsVerticalTScale.configure('ds.Vertical.TScale', font=fontNormal)
+
+    # Style for Scrollbar
+    global styleDsHorizontalTScrollbar
+    styleDsHorizontalTScrollbar = ttk.Style()
+    styleDsHorizontalTScrollbar.configure('ds.Horizontal.TScrollbar', font=fontNormal)
+    global styleDsVerticalTScrollbar
+    styleDsVerticalTScrollbar = ttk.Style()
+    styleDsVerticalTScrollbar.configure('ds.Vertical.TScrollbar', font=fontNormal)
+
+    # Style for Separator
+    global styleDsTSeparator
+    styleDsTSeparator = ttk.Style()
+    styleDsTSeparator.configure('ds.TSeparator', font=fontNormal)
+
+    # Style for Sizegrip
+    global styleDsTSizegrip
+    styleDsTSizegrip = ttk.Style()
+    styleDsTSizegrip.configure('ds.TSizegrip', font=fontNormal)
+
+    # Style for Treeview
+    global styleDsTreeview
+    styleDsTreeview = ttk.Style()
+    styleDsTreeview.configure('ds.Treeview', font=fontNormal)
+
     resetFontSize()
-    root.option_add('*TCombobox*Listbox.font', fontNormal)
+    result.option_add('*TCombobox*Listbox.font', fontNormal)
+    return result
 
 # Manage font size.
 
@@ -180,3 +228,22 @@ def resetFontSize() -> None:
         initsize = 10
     for currFont in [fontNormal, fontFixed]:
         currFont.configure(size=max(1, initsize))
+
+
+def noUserInput_init(widget):
+    widget.configure(state=tk.NORMAL)
+    widget.bind('<1>', lambda event: widget.focus_set())
+    widget.configure(state=tk.DISABLED)
+    return widget
+
+
+def noUserInput_delete(widget, *args, **kwargs):
+    widget.configure(state=tk.NORMAL)
+    widget.delete(*args, **kwargs)
+    widget.configure(state=tk.DISABLED)
+
+
+def noUserInput_insert(widget, *args, **kwargs):
+    widget.configure(state=tk.NORMAL)
+    widget.insert(*args, **kwargs)
+    widget.configure(state=tk.DISABLED)
