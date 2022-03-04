4dd3421b5bf235941730b9345f849d8a9232e71b
Gustav Johansson <gustav.johansson@foi.se>
2022-03-04 14:22:38 +0100

    Cleaned up.

--- doorstop/cli/tests/tutorial.py
+++ doorstop/cli/tests/tutorial.py
@@ -45,15 +45,6 @@ class TestBase(unittest.TestCase):
         """Call 'doorstop' with a string of arguments."""
         print("$ doorstop {}".format(args))
         cmd = "{} {} -v".format(DOORSTOP, args)
-        print(
-            "##################################\n"
-            + "{n}".format(n=os.environ)
-            + "\n"
-            + "##################################\n"
-            + "{n}".format(n=cmd)
-            + "\n"
-            + "##################################\n"
-        )
         cp = subprocess.run(cmd, shell=True, stdout=stdout, stderr=subprocess.PIPE)
         if cp.returncode != returncode:
             raise AssertionError("command failed: doorstop {}".format(args))
