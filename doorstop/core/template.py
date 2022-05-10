e410e277ef57562d74aa3ca97ecb758ff6e98041
Gustav Johansson <gustav@neer.se>
2022-05-10 17:52:35 +0200

    Always copy template folder.

--- doorstop/core/template.py
+++ doorstop/core/template.py
@@ -29,14 +29,29 @@ def get_template(obj, path, ext, template):
     # Set assets, ouput and template folders.
     if is_tree(obj):
         assets_dir = os.path.join(path, Document.ASSETS)  # path is a directory name
+        document_template = obj.documents[0].template
         template_dir = os.path.join(path, "template")
         output_dir = path
     else:
         assets_dir = os.path.join(
             os.path.dirname(path), Document.ASSETS
         )  # path is a filename
+        document_template = obj.template
         template_dir = os.path.join(os.path.dirname(path), "template")
         output_dir = os.path.dirname(path)
+
+    # Check for custom template and verify that it is available.
+    if template and not document_template:
+        raise common.DoorstopError(
+            "Template flag set, but no 'template' folder was found."
+        )
+    if document_template and not template:
+        print("document_template = %s" % document_template)
+        print("template = %s" % template)
+        raise common.DoorstopError(
+            "'template' folder found, but template flag not set."
+        )
+
 
     # Get the builtin templates.
     template_assets = os.path.join(os.path.dirname(__file__), "files", "templates")
@@ -46,7 +61,7 @@ def get_template(obj, path, ext, template):
         template_assets = template_assets + "/markdown"
     elif ext == ".tex":
         template_assets = template_assets + "/latex"
-        builtin_template = "template/doorstop"
+        builtin_template = "doorstop"
     elif ext == ".txt":
         template_assets = template_assets + "/text"
     else:
@@ -65,8 +80,18 @@ def get_template(obj, path, ext, template):
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
 
+    # Copy template from document if it exists.
+    if document_template:
+        if not os.path.isdir(template_dir):
+            os.makedirs(template_dir)
+        log.info(
+            "Copying %s to %s",
+            document_template,
+            os.path.join(os.path.dirname(path), "template"),
+        )
+        common.copy_dir_contents(document_template, template_dir)
     # Only create template_dir if template actually exists.
-    if os.path.isdir(template_assets):
+    elif os.path.isdir(template_assets):
         if not os.path.isdir(template_dir):
             os.makedirs(template_dir)
         log.info(
