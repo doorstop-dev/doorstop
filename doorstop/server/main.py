ab29ee0a181632f6f4e9e519734cf54ecfb768f1
Gustav Wulf <gustav@neer.se>
2023-12-30 10:40:40 +0100

    Blacked

--- doorstop/server/main.py
+++ doorstop/server/main.py
@@ -232,12 +232,7 @@ def get_attr(prefix, uid, name):
 def get_template(filename):
     """Serve static files. Mainly used to serve CSS files and javascript."""
     public_dir = os.path.join(
-        os.path.dirname(__file__),
-        "..",
-        "core",
-        "files",
-        "templates",
-        "html"
+        os.path.dirname(__file__), "..", "core", "files", "templates", "html"
     )
     return bottle.static_file(filename, root=public_dir)
 
